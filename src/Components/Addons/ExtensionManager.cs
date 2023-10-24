b9fc417af7af5fbb0dbefad590c3b68444a654fe
raul <raulssorban@gmail.com>
2023-10-24 19:59:28 +0200

    Finalized implementation
    (cherry picked from commit faaf870ab34dd8a2db842c7567448f3045d4268e)

--- src/Components/Addons/ExtensionManager.cs
+++ src/Components/Addons/ExtensionManager.cs
@@ -7,6 +7,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using API.Assembly;
 using API.Events;
+using Carbon;
 using Facepunch.Extend;
 using Loaders;
 using Mono.Cecil;
@@ -17,7 +18,7 @@ using Logger = Utility.Logger;
 
 /*
  *
- * Copyright (c) 2022-2023 Carbon Community 
+ * Copyright (c) 2022-2023 Carbon Community
  * All rights reserved.
  *
  */
@@ -30,7 +31,7 @@ internal sealed class ExtensionManager : AddonManager
 	/*
 	 * CARBON EXTENSIONS
 	 * API.Contracts.ICarbonExtension
-	 * 
+	 *
 	 * An assembly to be considered as a Carbon Extension must:
 	 *   1. Implement the ICarbonExtension interface
 	 *   2. Must not change directly the world
@@ -111,15 +112,15 @@ internal sealed class ExtensionManager : AddonManager
 
 			OnFileCreated = (sender, file) =>
 			{
-				Reload(file, "ExtensionManager.Created");
+				Load(file, "ExtensionManager.Created");
 			},
 			OnFileChanged = (sender, file) =>
 			{
-				Reload(file, "ExtensionManager.Changed");
+				Load(file, "ExtensionManager.Changed");
 			},
 			OnFileDeleted = (sender, file) =>
 			{
-				Reload(file, "ExtensionManager.Deleted");
+				Load(file, "ExtensionManager.Deleted");
 			}
 		});
 	}
@@ -142,7 +143,7 @@ internal sealed class ExtensionManager : AddonManager
 			{
 				case ".dll":
 					IEnumerable<Type> types;
-					Assembly asm = _loader.Load(file, requester, _directories, blacklist, whitelist)?.Assembly
+					Assembly asm = _loader.Load(file, requester, _directories, blacklist, whitelist, AssemblyLoader.ProcessTypes.Extension)?.Assembly
 						?? throw new ReflectionTypeLoadException(null, null, null);
 
 					if (AssemblyManager.IsType<ICarbonExtension>(asm, out types))
@@ -267,7 +268,17 @@ internal sealed class ExtensionManager : AddonManager
 					}
 					else
 					{
-						var stream = new MemoryStream(Process(File.ReadAllBytes(file)));
+						var raw = File.ReadAllBytes(file);
+						var convert = Community.Runtime.Compat.AttemptOxideConvert(ref raw);
+
+						switch (convert)
+						{
+							case ConversionResult.Fail:
+								Logger.Warn($" >> Failed Oxide extension conversion for '{file}'");
+								return;
+						}
+
+						var stream = new MemoryStream(Process(raw));
 						var assembly = Mono.Cecil.AssemblyDefinition.ReadAssembly(stream, new ReaderParameters { AssemblyResolver = new Resolver() });
 						var originalName = assembly.Name.Name;
 						assembly.Name = new AssemblyNameDefinition($"{assembly.Name.Name}_{Guid.NewGuid()}", assembly.Name.Version);
