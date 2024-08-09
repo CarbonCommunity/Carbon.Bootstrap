5d6096c481ab7b1eba8fd5d6b8850459f08fdf72
raul <raulssorban@gmail.com>
2024-08-09 18:02:48 +0200

    Load all plugins before Asset Warmup and only load Asset Warmup when all Carbon systems are online (same logic applies to `skipassetwarmup_crashes`) This solves an issue where 7950x/7950x3d hardware loads Asset Warmup before Carbon's systems causing weird behavior

---
+++ src/Patches/FileSystem_WarmupHalt.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using Carbon;
+using Carbon.Core;
+using HarmonyLib;
+
+namespace Patches;
+
+[HarmonyPatch(typeof(FileSystem_Warmup), nameof(FileSystem_Warmup.Run), new System.Type[] { typeof(string[]), typeof(Action<string>), typeof(string), typeof(int) })]
+internal static class FileSystem_WarmupHalt
+{
+	internal static bool IsReady = false;
+	internal static bool AllowNative = false;
+
+	internal static bool ShouldGo => ModLoader.IsBatchComplete || Community.AllProcesses == 0;
+
+	public static IEnumerator Process(string[] assetList, Action<string> statusFunction = null, string format = null, int priority = 0)
+	{
+		while (!IsReady || !ShouldGo)
+		{
+			yield return null;
+		}
+
+		AllowNative = true;
+
+		yield return FileSystem_Warmup.Run(assetList, statusFunction, format, priority);
+	}
+
+	public static bool Prefix(string[] assetList, Action<string> statusFunction, string format, int priority, ref IEnumerator __result)
+	{
+		if (AllowNative || (IsReady && ShouldGo))
+		{
+			return true;
+		}
+
+		__result = Process(assetList, statusFunction, format, priority);
+		return false;
+	}
+}
