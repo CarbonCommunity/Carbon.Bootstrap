1a2f79bb03665245cc05bcef3e5703b416baf624
raul <raulssorban@gmail.com>
2025-06-02 19:00:02 +0200

    Context cleanup

--- src/Utility/Context.cs
+++ src/Utility/Context.cs
@@ -60,7 +60,7 @@ internal sealed class Context
 			CarbonPlugins = Switches.GetScriptDir(Path.Combine(Carbon, "plugins"));
 			if (!Directory.Exists(CarbonPlugins)) Directory.CreateDirectory(CarbonPlugins);
 
-			CarbonConfig = Path.Combine(Carbon, "config.json");
+			CarbonConfig = Path.Combine(Switches.GetCarbonConfigDir(Carbon), "config.json");
 		}
 		catch (System.Exception e)
 		{
